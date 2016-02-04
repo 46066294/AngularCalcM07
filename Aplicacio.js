287fe017edc2353be425212c6e4e4b736e15beee
46066294p <46066294p@iespoblenou.org>
2016-02-04 16:59:12 +0100

    Initial commit

---
+++ Aplicacio.js
@@ -0,0 +1,5 @@
+/**
+ * Created by 46066294p on 04/02/16.
+ */
+
+var aplicacio = angular.module("myApp", []);
---
+++ Controller.js
@@ -0,0 +1,7 @@
+/**
+ * Created by 46066294p on 04/02/16.
+ */
+
+Aplicacio.controller('myController', function($scope){
+
+});
